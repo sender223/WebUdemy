00ad7a609f441cf4eee603ab68e88274efd01799
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2020-07-22 17:19:39 -0300

    Servico de Popular o banco de dados

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -36,12 +36,16 @@ namespace SalesWebMvc
     services.AddDbContext<SalesWebMvcContext>(options =>
             options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder => 
             builder.MigrationsAssembly("SalesWebMvc")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService) {
+            //se estiver no perfil de desenvolvimento
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else {
                 app.UseExceptionHandler("/Home/Error");
