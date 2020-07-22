e3aa60221fd96768048c5b2b936034dad49db3e7
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2020-07-22 17:42:47 -0300

    Controle Vendedor

---
+++ SalesWebMvc/Controllers/VendedorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class VendedorController : Controller
+    {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
